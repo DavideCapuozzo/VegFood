daf9525625b50022c8fd4267c85a59cacd41de07
DavideCapuozzo <devsoloweb@gmail.com>
2024-10-18 23:25:17 +0200

    Tabs typescript fix

--- src/component/Tabs.tsx
+++ src/component/Tabs.tsx
@@ -1,43 +1,51 @@
-/* https://www.devwares.com/blog/how-to-create-react-tabs-with-tailwind-css/ */
-import { useState } from 'react';
+import { useState, ReactElement } from 'react';
+import { TabsProps, TabProps } from "../interface/Interface"; // Assicurati che questi siano definiti correttamente
+import React from 'react';
 
-const Tabs = ({ children }) => {
-    
-    const [activeTab, setActiveTab] = useState(children[0].props.label);
+const Tabs = ({ children }: TabsProps) => {
+    // Controlla se ci sono children e assegna un valore predefinito
+    const initialActiveTab = React.Children.count(children) > 0 ? (children as ReactElement[])[0].props.label : "";
+    const [activeTab, setActiveTab] = useState(initialActiveTab);
 
-    const handleClick = (e, newActiveTab) => {
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>, newActiveTab: string) => {
         e.preventDefault();
         setActiveTab(newActiveTab);
     };
 
     return (
-            <div className="max-w-full mx-auto bg-white">
-                <div className="flex border-gray-300 bg-[#F8F8F8] h-20">
-                    {children.map(child => (
-                    <button
-                        key={child.props.label}
-                        className={`${ activeTab === child.props.label ? 'underline underline-offset-1' : ''} flex-1 py-2 hover:outline-none hover:border-[#F8F8F8] focus:outline-none focus:border-none hover:text-slate-950`}
-                        onClick={e => handleClick(e, child.props.label)}
-                    >
-                        {child.props.label}
-                    </button>
-                    ))}
-                </div>
-                <div className="pt-8 pb-28 px-5 lg:px-36 xl:px-64 my-auto min-h-[500px]">
-                    {children.map(child => {
-                    if (child.props.label === activeTab) {
+        <div className="max-w-full mx-auto bg-white">
+            <div className="flex border-gray-300 bg-[#F8F8F8] h-20">
+                {React.Children.map(children, (child) => {
+                    // Verifica che child sia un ReactElement
+                    if (React.isValidElement(child)) {
+                        return (
+                            <button
+                                key={child.props.label}
+                                className={`${activeTab === child.props.label ? 'underline underline-offset-1' : ''} flex-1 py-2 hover:outline-none hover:border-[#F8F8F8] focus:outline-none focus:border-none hover:text-slate-950`}
+                                onClick={(e) => handleClick(e, child.props.label)}
+                            >
+                                {child.props.label}
+                            </button>
+                        );
+                    }
+                    return null;
+                })}
+            </div>
+            <div className="pt-8 pb-28 px-5 lg:px-36 xl:px-64 my-auto min-h-[500px]">
+                {React.Children.map(children, (child) => {
+                    if (React.isValidElement(child) && child.props.label === activeTab) {
                         return <div key={child.props.label}>{child.props.children}</div>;
                     }
                     return null;
-                    })}
-                </div>
+                })}
             </div>
-        );
+        </div>
+    );
 };
 
-const Tab = ({ label, children }) => {
+const Tab = ({ children }: TabProps) => {
     return (
-        <div label={label} className="hidden">
+        <div className="hidden">
             {children}
         </div>
     );
