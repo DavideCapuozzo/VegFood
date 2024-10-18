315ac345e362e6e4a87aac504aa7989a1317b5ed
DavideCapuozzo <devsoloweb@gmail.com>
2024-10-18 22:20:09 +0200

    apiSlice typescript fix

--- src/store/apiSlice.tsx
+++ src/store/apiSlice.tsx
@@ -1,21 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IBookmark } from '../interface/Interface'; 
+
+interface ApiState {
+    value: IBookmark[][]; 
+}
+
+const initialState: ApiState = {
+    value: [],
+};
 
 export const apiSlice = createSlice({
     name: 'api',
-    initialState:{
-        value: []
-    },
-    reducers:{
-        addSearch:(state, action) => {
-            state.value.push(action.payload);
-            console.log('sono in slice api', apiSlice)
+    initialState,
+    reducers: {
+        addSearch: (state, action: PayloadAction<IBookmark[]>) => {
+            state.value.push(action.payload); 
+            console.log('sono in slice api', apiSlice);
         },
-        removeSearch:(state) =>{
+        
+        removeSearch: (state) => {
             state.value = [];
-        }
-    }
-})
+        },
+    },
+});
 
-export const { addSearch, removeSearch } = apiSlice.actions
-
-export const apiReducer = apiSlice.reducer
+export const { addSearch, removeSearch } = apiSlice.actions;
+export const apiReducer = apiSlice.reducer;
