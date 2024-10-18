facd2cfd4675e9852071f809a1aa4f43465aa463
DavideCapuozzo <devsoloweb@gmail.com>
2024-10-18 23:47:33 +0200

    main typescript fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,39 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-import { Provider } from 'react-redux'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import store from './store/store.js'
-import Recipes from './page/Recipes.jsx'
-import Bookmarks from './page/Bookmarks.jsx'
-import DetailsRecipes from './page/DetailsRecipes.js'
+import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import store from '../src/store/store.js';
+import Recipes from './page/Recipes.jsx';
+import Bookmarks from './page/Bookmarks.jsx';
+import DetailsRecipes from './page/DetailsRecipes.js';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App></App>
+    element: <App />,
   },
   {
     path: "/recipes",
-    element: <Recipes></Recipes>
+    element: <Recipes />,
   },
   {
     path: "/bookmarks",
-    element: <Bookmarks></Bookmarks>
+    element: <Bookmarks />,
   },
   {
     path: "/details/:detailsID",
-    element: <DetailsRecipes></DetailsRecipes>
+    element: <DetailsRecipes />,
   },
-  
-])
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router}></RouterProvider>
-    </Provider>
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </React.StrictMode>
+  );
+} else {
+  console.error("Element with id 'root' not found.");
+}
